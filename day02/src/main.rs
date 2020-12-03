af8b86104f83fd636b6c67eb7db2f041f6ee1535
Joey Steele <joeysteele2001@gmail.com>
2020-12-02 21:01:44 -0500

    Day 2, Part 1

--- day02/src/main.rs
+++ day02/src/main.rs
@@ -1,3 +1,19 @@
+use day02::PasswordDBEntry;
+
+const INPUT: &str = include_str!("../input.txt");
+
 fn main() {
-    println!("Hello, world!");
+    part1()
+}
+
+fn part1() {
+    let entries = INPUT.lines()
+        .map(|l| l.parse::<PasswordDBEntry>().unwrap());
+
+    let num_valid = entries
+        .filter(|e| e.is_valid())
+        .count();
+
+    println!("*** PART 1 ***");
+    println!("{}", num_valid);
 }
