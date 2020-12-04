4016090dae8d5fd60e7f32f4bacd2488bbf57419
Joey Steele <joeysteele2001@gmail.com>
2020-12-03 20:16:38 -0500

    Day03 Part 2 functional

--- day03/src/main.rs
+++ day03/src/main.rs
@@ -1,7 +1,8 @@
 const INPUT: &str = include_str!("../input.txt");
 
 fn main() {
-    part1()
+    part1();
+    part2();
 }
 
 fn parse_input(input: &str) -> Vec<Vec<bool>> {
@@ -40,4 +41,40 @@ fn part1() {
     }
 
     println!("{}", count);
+}
+
+fn count_trees(map: &Vec<Vec<bool>>, dx: usize, dy: usize) -> usize {
+    let n_rows = map.len();
+    let n_cols = map[0].len(); // assume all rows are the same length
+
+    let mut count = 0;
+    let mut j = 0;
+    let mut i = 0;
+
+    while i < n_rows - 1 {
+        i += dy;
+        j += dx;
+
+        if map[i][j % n_cols] {
+            // This is a tree
+            count += 1;
+        }
+    }
+
+    return count;
+}
+
+fn part2() {
+    println!("*** PART 2 ***");
+
+    let map = parse_input(INPUT);
+
+    let slopes = [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)];
+
+    let product: usize = slopes
+        .iter()
+        .map(|&(dx, dy)| count_trees(&map, dx, dy))
+        .product();
+
+    println!("{}", product)
 }
