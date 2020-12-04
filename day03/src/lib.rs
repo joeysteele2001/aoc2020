ff1c3010d6e22ac1c75ca37d4d64043c0ac0f9d4
Joey Steele <joeysteele2001@gmail.com>
2020-12-03 20:47:21 -0500

    Make Map struct for tree counting

---
+++ day03/src/lib.rs
@@ -0,0 +1,84 @@
+use std::str::FromStr;
+
+#[derive(Clone, Debug, PartialEq)]
+pub struct Map(Vec<Vec<bool>>);
+
+impl Map {
+    pub fn count_trees(&self, dx: usize, dy: usize) -> usize {
+        let mut count = 0;
+        let mut j = 0;
+        let mut i = 0;
+
+        while i < self.n_rows() - 1 {
+            i += dy;
+            j += dx;
+
+            if self.0[i][j % self.n_cols()] {
+                // This is a tree
+                count += 1;
+            }
+        }
+
+        return count;
+    }
+
+    pub fn parse_string(input: &str) -> Option<Self> {
+        let col_length = match input.lines().next() {
+            Some(line) => line.len(),
+            None => {
+                // This is an empty string, so return an empty map
+                return Some(Self(vec![vec![]]));
+            }
+        };
+
+        let mut rows = vec![];
+
+        for line in input.lines() {
+            if line.len() != col_length {
+                return None;
+            }
+
+            let mut row = Vec::with_capacity(col_length);
+            Self::parse_row_from_str(line, &mut row)?;
+            rows.push(row);
+        }
+
+        Some(Self(rows))
+    }
+
+    #[must_use]
+    fn parse_row_from_str(s: &str, row: &mut Vec<bool>) -> Option<()> {
+        let chars = s.lines().next()?.chars();
+
+        for c in chars {
+            let is_tree = match c {
+                '.' => false,
+                '#' => true,
+                _ => {
+                    // Invalid character
+                    return None;
+                }
+            };
+
+            row.push(is_tree);
+        }
+
+        Some(())
+    }
+
+    fn n_rows(&self) -> usize {
+        self.0.len()
+    }
+
+    fn n_cols(&self) -> usize {
+        self.0.get(0).map(Vec::len).unwrap_or(0)
+    }
+}
+
+impl FromStr for Map {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        Self::parse_string(s).ok_or(())
+    }
+}
--- day03/src/main.rs
+++ day03/src/main.rs
@@ -1,3 +1,5 @@
+use day03::Map;
+
 const INPUT: &str = include_str!("../input.txt");
 
 fn main() {
@@ -5,62 +7,25 @@ fn main() {
     part2();
 }
 
-fn parse_input(input: &str) -> Vec<Vec<bool>> {
-    input
-        .lines()
-        .map(|l| {
-            l.chars()
-                .map(|c| match c {
-                    '.' => false,
-                    '#' => true,
-                    _ => panic!("bad character"),
-                })
-                .collect()
-        })
-        .collect()
-}
-
 fn part1() {
     println!("*** PART 1 ***");
 
-    let map = parse_input(INPUT);
-
-    let count = count_trees(&map, 3, 1);
+    let map = INPUT.parse::<Map>().unwrap();
+    let count = map.count_trees(3, 1);
 
     println!("{}", count);
-}
-
-fn count_trees(map: &Vec<Vec<bool>>, dx: usize, dy: usize) -> usize {
-    let n_rows = map.len();
-    let n_cols = map[0].len(); // assume all rows are the same length
-
-    let mut count = 0;
-    let mut j = 0;
-    let mut i = 0;
-
-    while i < n_rows - 1 {
-        i += dy;
-        j += dx;
-
-        if map[i][j % n_cols] {
-            // This is a tree
-            count += 1;
-        }
-    }
-
-    return count;
 }
 
 fn part2() {
     println!("*** PART 2 ***");
 
-    let map = parse_input(INPUT);
+    let map = INPUT.parse::<Map>().unwrap();
 
     let slopes = [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)];
 
     let product: usize = slopes
         .iter()
-        .map(|&(dx, dy)| count_trees(&map, dx, dy))
+        .map(|&(dx, dy)| map.count_trees(dx, dy))
         .product();
 
     println!("{}", product)
