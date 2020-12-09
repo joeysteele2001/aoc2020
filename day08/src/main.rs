b5b33cad758a1d9725aa199f3ca9cb85245678c7
Joey Steele <joeysteele2001@gmail.com>
2020-12-08 21:00:26 -0500

    Part 1 functional

--- day08/src/main.rs
+++ day08/src/main.rs
@@ -1,3 +1,17 @@
+use day08::*;
+
+const INPUT: &str = include_str!("../input.txt");
+
 fn main() {
-    println!("Hello, world!");
+    part1();
+}
+
+fn part1() {
+    println!("*** PART 1 ***");
+
+    let instructions = parse_instructions(INPUT);
+    let mut vm = Vm::new(instructions);
+    vm.step_until_loop();
+
+    println!("{}", vm.acc());
 }
