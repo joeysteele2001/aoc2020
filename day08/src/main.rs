bdcda151ce8e42d645d985187184bf1de0f84f27
Joey Steele <joeysteele2001@gmail.com>
2020-12-08 21:22:34 -0500

    Day08 part2 functional

--- day08/src/main.rs
+++ day08/src/main.rs
@@ -4,6 +4,7 @@ const INPUT: &str = include_str!("../input.txt");
 
 fn main() {
     part1();
+    part2();
 }
 
 fn part1() {
@@ -14,4 +15,34 @@ fn part1() {
     vm.step_until_loop();
 
     println!("{}", vm.acc());
+}
+
+fn part2() {
+    println!("*** PART 2 ***");
+
+    let original_instructions = parse_instructions(INPUT);
+
+    for (i, &ins) in original_instructions.iter().enumerate() {
+        let mut modified_instructions = original_instructions.clone();
+
+        match ins {
+            Instruction::Acc(_) => {
+                continue;
+            }
+
+            Instruction::Nop(x) => {
+                modified_instructions[i] = Instruction::Jmp(x);
+            }
+
+            Instruction::Jmp(x) => {
+                modified_instructions[i] = Instruction::Nop(x);
+            }
+        }
+
+        let mut vm = Vm::new(modified_instructions);
+
+        if vm.terminates() {
+            println!("{}", vm.acc());
+        }
+    }
 }
